8ad604f91ad2769ee98449d928900ce2512b2ca3
albetnovendo <ryxinfrvr@gmail.com>
2022-03-14 18:30:32 +0700

    add bulk and delete all

--- resources/views/livewire/tables/kerja/index.blade.php
+++ resources/views/livewire/tables/kerja/index.blade.php
@@ -8,6 +8,43 @@
                     onclick="location.href='{{ route('table.kerja.add') }}'">Create</button>
                 @include('livewire.tables.filter')
                 @include('livewire.tables.search')
+                @if ($bulkAction)
+                    <button class="btn btn-danger mt-2" wire:click='bulkDelete()'>Bulk Delete</button>
+                    <button class="btn btn-danger mt-2" wire:click="$emit('openModal', 'delAll')">Delete All
+                        Data</button>
+                    <div class="modal fade" wire:ignore.self wire:key='open-delall-modal' id="delAll" tabindex="-1"
+                        aria-labelledby="delAllLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="delAllLabel">Delete All Data
+                                    </h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="alert alert-danger shadow text-light">SEMUA DATA TABEL KERJA AKAN
+                                        DIHAPUS. DATA
+                                        TIDAK AKAN BISA DIPULIHKAN KEMBALI BILA KAMU MENGHAPUSNYA.</div>
+                                    <p>Konfirmasi Sudo:</p>
+                                    <form wire:submit.prevent='enterSudo'>
+                                        <input type="password" name="password"
+                                            class="form-control @error('password') is-invalid @elseif($password != '') is-valid @enderror"
+                                            {!! wireModel('password') !!}>
+                                        @error('password')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                        <button class="btn btn-sm btn-primary mt-1">Kirim</button>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
         <div class="card-body">
@@ -15,6 +52,7 @@
                 <table class="table">
                     <thead>
                         <tr>
+                            <th class="text-center">Select</th>
                             <th>No.</th>
                             <th>Nama</th>
                             <th>Jenis Kelamin</th>
@@ -29,6 +67,10 @@
                     <tbody>
                         @foreach ($kerjas as $kerja)
                             <tr>
+                                <form wire:submit.prevent>
+                                    <td class="text-center"><input type="checkbox" name="items" wire:model='items'
+                                            value="{{ $kerja->id }}" wire:key='select-{{ $kerja->id }}'></td>
+                                </form>
                                 <td>{{ ($kerjas->currentpage() - 1) * $kerjas->perpage() + $loop->index + 1 }}</td>
                                 <td>{{ $kerja->name }}</td>
                                 <td>{{ $kerja->jenis_kelamin == 'l' ? 'Pria' : 'Perempuan' }}</td>
